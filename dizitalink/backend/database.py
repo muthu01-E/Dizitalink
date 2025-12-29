8448a449ac802904f0819c7e143f839a4a3e0e2b
Esaimuthu <eesaimuthu@gmail.com>
2025-12-29 16:29:09 +0530

    deleted backend

--- dizitalink/backend/database.py
+++
@@ -1,7 +0,0 @@
-from pymongo import MongoClient
-
-MONGO_URL = "mongodb+srv://eesaimuthu_db_user:SOgzcCMb0SZROAYd@auth.kwosr2d.mongodb.net/"
-client = MongoClient(MONGO_URL)
-
-db = client["dizitalink"]
-users_collection = db["users"]
